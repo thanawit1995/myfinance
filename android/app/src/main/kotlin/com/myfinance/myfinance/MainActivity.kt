307b7f4a2c0334af228255c48c681ed27de17faa
JP Money User <user@jpmoney.local>
2026-09-21 17:41:10 +0700

    fix(android): use FlutterFragmentActivity, AppCompat theme and biometric permissions

--- android/app/src/main/kotlin/com/myfinance/myfinance/MainActivity.kt
+++ android/app/src/main/kotlin/com/myfinance/myfinance/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.myfinance.myfinance
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
